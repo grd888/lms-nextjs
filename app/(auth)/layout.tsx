e8bdb243b5ca2d235ee8b4cc39ab034cef3a9ca9
Greg Delgado <grd888@gmail.com>
2026-01-11 18:42:35 +0800

    add a back button to the login screen

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,7 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
 export default function AuthLayout({
   children,
 }: {
@@ -5,6 +9,16 @@ export default function AuthLayout({
 }) {
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center">
+      <Link
+        href="/"
+        className={buttonVariants({
+          variant: "outline",
+          className: "absolute top-4 left-4",
+        })}
+      >
+        <ArrowLeft className="size-4" />
+        Back
+      </Link>
       <div className="flex w-full max-w-sm flex-col gap-6">{children}</div>
     </div>
   );
