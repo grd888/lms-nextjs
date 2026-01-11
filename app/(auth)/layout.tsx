b019dcf2a0d87a9f674ea9c8eab5e4d5d6b81867
Greg Delgado <grd888@gmail.com>
2026-01-11 19:09:10 +0800

    add logo and privacy links

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,8 @@
 import { buttonVariants } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/public/logo.png";
 
 export default function AuthLayout({
   children,
@@ -19,7 +21,26 @@ export default function AuthLayout({
         <ArrowLeft className="size-4" />
         Back
       </Link>
-      <div className="flex w-full max-w-sm flex-col gap-6">{children}</div>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <Image
+            src={Logo}
+            alt="Logo"
+            width={32}
+            height={32}
+          />
+          Learnability
+        </Link>
+        {children}
+        <div className="text-balance text-center text-xs text-muted-foreground">
+          By clicking continue, you agree to our{" "}
+          <span className="hover:text-primary hover:underline">Terms of Service</span> and{" "}
+          <span className="hover:text-primary hover:underline">Privacy Policy</span>
+        </div>
+      </div>
     </div>
   );
 }
