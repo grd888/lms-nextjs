32341496f09a8c8f1d739e82d2903717d6be409f
Greg Delgado <grd888@gmail.com>
2026-01-11 18:00:40 +0800

    Create login form

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-2xl text-red-500">Hello World</h1>
+      <ThemeToggle />
     </div>
   );
 }
