5185492361ac3e4e47755f0405bd6d3fe3ead6fe
Greg Delgado <grd888@gmail.com>
2026-01-12 16:53:53 +0800

    sign in with github

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -8,9 +9,31 @@ import {
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { GithubIcon } from "lucide-react";
+import { GithubIcon, Loader } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
+import React from "react";
 
 export default function LoginPage() {
+  const [githubPending, startGithubTransition] = React.useTransition();
+
+  async function signInWithGithub() {
+    startGithubTransition(async () => {
+      await authClient.signIn.social({
+        provider: "github",
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Signed in with GitHub");
+          },
+          onError: () => {
+            toast.error("Internal Server Error");
+          },
+        },
+      });
+    });
+  }
+
   return (
     <Card>
       <CardHeader>
@@ -20,11 +43,22 @@ export default function LoginPage() {
 
       <CardContent className="flex flex-col gap-4">
         <Button
+          onClick={signInWithGithub}
           className="w-full"
           variant="outline"
+          disabled={githubPending}
         >
-          <GithubIcon className="size-4" />
-          Sign in with GitHub
+          {githubPending ? (
+            <>
+              <Loader className="size-4 animate-spin" />
+              <span>Loading ...</span>
+            </>
+          ) : (
+            <>
+              <GithubIcon className="size-4" />
+              Sign in with GitHub
+            </>
+          )}
         </Button>
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
           <span className="relative z-10 bg-card px-2 text-muted-foreground">
@@ -35,7 +69,10 @@ export default function LoginPage() {
         <div className="grid gap-3">
           <div className="grid gap-2">
             <Label>Email</Label>
-            <Input type="email" placeholder="name@website.com" />
+            <Input
+              type="email"
+              placeholder="name@website.com"
+            />
           </div>
         </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   return (
     <div>
       <h1 className="text-2xl text-red-500">Hello World</h1>
+      {session?.user ? (
+        <p>Logged in as {session.user.name}</p>
+      ) : (
+        <p>Not logged in</p>
+      )}
       <ThemeToggle />
     </div>
   );
