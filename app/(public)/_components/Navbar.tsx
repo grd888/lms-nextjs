503a6e8e3b933ba7fda55881441901c9952da793
Greg Delgado <grd888@gmail.com>
2026-01-17 12:09:21 +0800

    add login and get started button

--- app/(public)/_components/Navbar.tsx
+++ app/(public)/_components/Navbar.tsx
@@ -1,7 +1,10 @@
+"use client"
 import Link from "next/link";
 import Logo from "@/public/logo.png";
 import Image from "next/image";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { authClient } from "@/lib/auth-client";
+import { buttonVariants } from "@/components/ui/button";
 
 const navigationItems = [
   { name: "Home", href: "/" },
@@ -10,10 +13,14 @@ const navigationItems = [
 ];
 
 export function Navbar() {
+  const { data: session, isPending } = authClient.useSession();
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur-[backdrop-fiter]:bg-background/60">
       <div className="container flex min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8">
-        <Link href="/" className="flex items-center space-x-2 mr-4">
+        <Link
+          href="/"
+          className="flex items-center space-x-2 mr-4"
+        >
           <Image
             src={Logo}
             alt="Logo"
@@ -25,13 +32,35 @@ export function Navbar() {
         <nav className="hidden md:flex md:flex-1 md:items-center md:justify-between">
           <div className="flex items-center space-x-2">
             {navigationItems.map((item) => (
-              <Link key={item.name} href={item.href} className="text-sm font-medium transition-colors hover:text-primary">
+              <Link
+                key={item.name}
+                href={item.href}
+                className="text-sm font-medium transition-colors hover:text-primary"
+              >
                 {item.name}
               </Link>
             ))}
           </div>
           <div>
             <ThemeToggle />
+            {isPending ? null : session ? (
+              <p>Logged in</p>
+            ) : (
+              <>
+                <Link
+                  href="/login"
+                  className={buttonVariants({ variant: "secondary" })}
+                >
+                  Login
+                </Link>
+                <Link
+                  href="/login"
+                  className={buttonVariants()}
+                >
+                  Get Started
+                </Link>
+              </>
+            )}
           </div>
         </nav>
       </div>
